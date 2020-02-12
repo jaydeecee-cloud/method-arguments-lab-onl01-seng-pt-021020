c6887115bd9ee872b39ca5d2d15a542aa55a9391
Joseph Choi <jdc07@live.com>
2020-02-12 21:57:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction with language (#introduction_with_language)
+def introduction with language (introduction_with_language)
   puts "Hi,my name is #{name} and I am learning to program in #{language}."
 end
